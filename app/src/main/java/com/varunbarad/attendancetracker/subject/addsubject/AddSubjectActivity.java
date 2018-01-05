668593a70ae582b5aa8f4c1ecb3175ad5ee92d17
Varun Barad <barad.varun29@gmail.com>
2018-01-05 16:23:35 +0530

    Create UI for add-subject screen.

---
+++ app/src/main/java/com/varunbarad/attendancetracker/subject/addsubject/AddSubjectActivity.java
@@ -0,0 +1,15 @@
+package com.varunbarad.attendancetracker.subject.addsubject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.varunbarad.attendancetracker.R;
+
+public class AddSubjectActivity extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_subject);
+  }
+}
