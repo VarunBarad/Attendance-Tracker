75f765e7bdc5e281fe4fa14864cd37558cb2fd32
Varun Barad <barad.varun29@gmail.com>
2018-01-05 13:22:53 +0530

    Create UI for subjects-list screen.

---
+++ app/src/main/java/com/varunbarad/attendancetracker/subject/listsubjects/SubjectsListActivity.java
@@ -0,0 +1,15 @@
+package com.varunbarad.attendancetracker.subject.listsubjects;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.varunbarad.attendancetracker.R;
+
+public class SubjectsListActivity extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_subjects_list);
+  }
+}
