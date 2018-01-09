28095557eb8a2c5ee5b59a17bf368e3a82303cc5
Varun Barad <barad.varun29@gmail.com>
2018-01-09 10:55:41 +0530

    Create UI & adapter of activity launched to select subject.

---
+++ app/src/main/java/com/varunbarad/attendancetracker/widget/subjectselect/SubjectSelectActivity.java
@@ -0,0 +1,15 @@
+package com.varunbarad.attendancetracker.widget.subjectselect;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.varunbarad.attendancetracker.R;
+
+public class SubjectSelectActivity extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_subject_select);
+  }
+}
---
+++ app/src/main/java/com/varunbarad/attendancetracker/widget/subjectselect/SubjectsAdapter.java
@@ -0,0 +1,75 @@
+package com.varunbarad.attendancetracker.widget.subjectselect;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.varunbarad.attendancetracker.data.model.Subject;
+import com.varunbarad.attendancetracker.databinding.ListItemSubjectBinding;
+
+import java.util.ArrayList;
+
+/**
+ * Creator: Varun Barad
+ * Date: 09-01-2018
+ * Project: AttendanceTracker
+ */
+public class SubjectsAdapter extends RecyclerView.Adapter<SubjectsAdapter.ViewHolder> {
+  private Context context;
+  private ArrayList<Subject> subjects;
+  
+  public SubjectsAdapter(Context context, ArrayList<Subject> subjects) {
+    this.context = context;
+    this.subjects = subjects;
+  }
+  
+  @Override
+  public SubjectsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+    ListItemSubjectBinding itemBinding =
+        ListItemSubjectBinding.inflate(inflater, parent, false);
+    
+    return new SubjectsAdapter.ViewHolder(itemBinding);
+  }
+  
+  @Override
+  public void onBindViewHolder(SubjectsAdapter.ViewHolder holder, int position) {
+    holder.bind(this.subjects.get(position));
+  }
+  
+  @Override
+  public int getItemCount() {
+    if (this.subjects != null) {
+      return this.subjects.size();
+    } else {
+      return 0;
+    }
+  }
+  
+  public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private ListItemSubjectBinding itemBinding;
+    
+    public ViewHolder(ListItemSubjectBinding itemBinding) {
+      super(itemBinding.getRoot());
+      
+      this.itemBinding = itemBinding;
+      
+      this.itemBinding
+          .getRoot()
+          .setOnClickListener(this);
+    }
+    
+    private void bind(Subject subject) {
+      this.itemBinding
+          .subjectName
+          .setText(subject.getName());
+    }
+    
+    @Override
+    public void onClick(View view) {
+      //ToDo: Handle click on subject
+    }
+  }
+}
