cb50cd3c87fc2cc1a7805b9415b43220e722d9bb
Varun Barad <barad.varun29@gmail.com>
2018-01-13 01:17:20 +0530

    Add menu to main-attendance screen.

--- app/src/main/java/com/varunbarad/attendancetracker/mainattendance/MainAttendanceActivity.java
+++ app/src/main/java/com/varunbarad/attendancetracker/mainattendance/MainAttendanceActivity.java
@@ -1,15 +1,30 @@
 package com.varunbarad.attendancetracker.mainattendance;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import com.varunbarad.attendancetracker.R;
+import com.varunbarad.attendancetracker.databinding.ActivityMainAttendanceBinding;
 
 public class MainAttendanceActivity extends AppCompatActivity {
+  private ActivityMainAttendanceBinding dataBinding;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main_attendance);
+    this.dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main_attendance);
+  
+    this.setSupportActionBar(this.dataBinding.toolbar);
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    this
+        .getMenuInflater()
+        .inflate(R.menu.main_attendance, menu);
+    
+    return true;
   }
 }
