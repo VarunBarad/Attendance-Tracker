15c2baa80d24ba3fb90bf1da43723afe43108c6f
Varun Barad <barad.varun29@gmail.com>
2018-02-22 21:52:28 +0530

    Change Ad-Provider from Google AdMob to InMobi Ads.

--- app/src/free/java/com/varunbarad/attendancetracker/mainattendance/AdHelper.kt
+++ app/src/free/java/com/varunbarad/attendancetracker/mainattendance/AdHelper.kt
@@ -2,8 +2,10 @@
 
 package com.varunbarad.attendancetracker.mainattendance
 
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
+import android.content.Context
+import android.widget.FrameLayout
+import com.inmobi.ads.InMobiBanner
+import com.inmobi.sdk.InMobiSdk
 import com.varunbarad.attendancetracker.BuildConfig
 import com.varunbarad.attendancetracker.databinding.ActivityMainAttendanceBinding
 
@@ -12,13 +14,28 @@ import com.varunbarad.attendancetracker.databinding.ActivityMainAttendanceBindin
  * Date: 18-02-2018
  * Project: AttendanceTracker
  */
-fun loadBannerAd(dataBinding: ActivityMainAttendanceBinding): Unit {
-    MobileAds.initialize(dataBinding.root.context, BuildConfig.AdMobAppId)
+fun initializeSdk(context: Context): Unit {
+    InMobiSdk.init(context, BuildConfig.InMobiAccountId)
+}
 
-    val adRequest = AdRequest.Builder()
-            .build()
+fun loadBannerAd(dataBinding: ActivityMainAttendanceBinding): Unit {
+    initializeSdk(dataBinding.root.context)
+
+    val bannerAd = InMobiBanner(
+            dataBinding.root.context,
+            BuildConfig.InMobiPlacementIdMainAttendance.toLong()
+    ).apply {
+        this.setRefreshInterval(60)
+    }
+
+    val layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+    )
 
     dataBinding
-            .adViewBanner
-            .loadAd(adRequest)
+            .adViewContainer
+            .addView(bannerAd, layoutParams)
+
+    bannerAd.load(dataBinding.root.context)
 }
